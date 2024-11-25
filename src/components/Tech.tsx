d540d92e3ff73edcecc30540931117b8f13db643
adelen99 <lupuadelin1999@yahoo.com>
2024-11-25 08:59:01 +0200

    Tech stack title added

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,16 +1,21 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
-
+import { styles } from "../styles";
 const Tech = (): React.ReactNode => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+    <>
+      <p className={styles.sectionSubText}>Technologies I work with</p>
+      <h2 className={styles.sectionHeadText}>Tech Stack.</h2>
+
+      <div className="flex flex-row flex-wrap justify-center mt-10 gap-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
